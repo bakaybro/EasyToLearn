0b3dc62924b11b8ef205fa35022c73907da8a4bc
bakai <bakaiberdibekov@gmail.com>
2023-01-29 09:15:31 +0200

    start

---
+++ src/main/java/com/example/easytolearn/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.example.easytolearn;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(EasyToLearnApplication.class);
+	}
+
+}
